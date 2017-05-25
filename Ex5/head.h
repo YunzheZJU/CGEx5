be8578a3553e46fe38dae7eb080ed53441cd80eb
Yunzhe <iLoveLive@honoka.moe>
2017-05-25 23:53:02 +0800

    Split functions into different cpp files. Rebuild the global variables by extern struct.

--- Ex5/head.h
+++ Ex5/head.h
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <iostream>
 #include <iomanip>
+
 // Include glut.h
 #include <gl/glut.h>
 
@@ -17,63 +18,71 @@ using namespace std;
 #define X 0
 #define Y 1
 #define Z 2
+
 // Define abbreviate for "radius" and "angle"
 #define R 0
 #define A 1
+
 // Define Pi in math
 #define Pi 3.1415926
 
-// Declare height and width variables for size of new window
-int wHeight = 0;							// Height of new window
-int wWidth = 0;								// Width of new window
+typedef struct {
+	// Declare height and width variables for size of new window
+	int wHeight = 0;							// Height of new window
+	int wWidth = 0;								// Width of new window
 
-// Initiate message string to be shown
-char message[70] = "Welcome!";				// Message string to be shown
+												// Initiate message string to be shown
+	char message[70] = "Welcome!";				// Message string to be shown
 
-// Initiate parameters for global and local rotating
-GLfloat fRotate = 0.0f;						// Global roatating
-GLfloat fTpRtt = 0.0f;						// Local rotating
+												// Initiate parameters for global and local rotating
+	GLfloat fRotate = 0.0f;						// Global roatating
+	GLfloat fTpRtt = 0.0f;						// Local rotating
 
-// Initiate switch of the scene
-GLboolean bpoint = GL_TRUE;					// Switch for point light
-GLboolean bspot = GL_TRUE;					// Switch for spot light
-GLboolean bPersp = GL_TRUE;					// Switch for perspective mode
-GLboolean bWire = GL_FALSE;					// Switch for line mode
-GLboolean bAnim = GL_FALSE;					// Switch for global rotating
-GLboolean bRtt = GL_FALSE;					// Switch for local rotating
+												// Initiate switch of the scene
+	GLboolean bpoint = GL_TRUE;					// Switch for point light
+	GLboolean bspot = GL_TRUE;					// Switch for spot light
+	GLboolean bPersp = GL_TRUE;					// Switch for perspective mode
+	GLboolean bWire = GL_FALSE;					// Switch for line mode
+	GLboolean bAnim = GL_FALSE;					// Switch for global rotating
+	GLboolean bRtt = GL_FALSE;					// Switch for local rotating
 
-// Initiate location of elements
-GLfloat teapot[] = { 0, 0, 0 };				// Position of teapot
-GLfloat camera[] = { 0, 1, 4 };				// Position of camera
-GLfloat camera_polar[] = { 4, 0 };			// Polar coordinates of camera
-GLfloat camera_target[] = { 0, 1, 0 };		// Position of target of camera
-GLfloat point[] = { 1, 1.75, 1, 1 };			// Position of point light
-GLfloat spot[] = { 0, 3, 0, 1 };			// Position of spot light
-GLfloat spot_target[] = { 0, 0, 0 };		// Position of target of spot light
+												// Initiate location of elements
+	GLfloat teapot[3] = { 0, 0, 0 };			// Position of teapot
+	GLfloat camera[3] = { 0, 1, 4 };			// Position of camera
+	GLfloat camera_polar[2] = { 4, 0 };			// Polar coordinates of camera
+	GLfloat camera_target[3] = { 0, 1, 0 };		// Position of target of camera
+	GLfloat point[4] = { 1, 1.75, 1, 1 };		// Position of point light
+	GLfloat spot[4] = { 0, 3, 0, 1 };			// Position of spot light
+	GLfloat spot_target[3] = { 0, 0, 0 };		// Position of target of spot light
 
-// Initiate list num for display list
-GLint List = 0;								// List num of display list
+												// Initiate list num for display list
+	GLint List = 0;								// List num of display list
 
-// Define colors for materials
-GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-GLfloat green[] = { 0.0f, 1.0f, 0.0f, 1.0f };
-GLfloat blue[] = { 0.0f, 0.0f, 1.0f, 1.0f };
-GLfloat yellow[] = { 1.0f, 1.0f, 0.0f, 1.0f };
-GLfloat turquoise[] = { 0.0f, 1.0f, 1.0f, 1.0f };
-GLfloat golden[] = { 1.0f, 0.843f, 0.0f, 1.0f };
-GLfloat white[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+												// Define colors for materials
+	GLfloat red[4] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	GLfloat green[4] = { 0.0f, 1.0f, 0.0f, 1.0f };
+	GLfloat blue[4] = { 0.0f, 0.0f, 1.0f, 1.0f };
+	GLfloat yellow[4] = { 1.0f, 1.0f, 0.0f, 1.0f };
+	GLfloat turquoise[4] = { 0.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat golden[4] = { 1.0f, 0.843f, 0.0f, 1.0f };
+	GLfloat white[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat black[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
-// Initiate cut off degree for spot light
-GLfloat spot_cutoff = 8.6f;					// Cut off degree of spot light
-// Define colors for lights
-GLfloat point_white[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-GLfloat point_red[] = { 1.0f, 0.2f, 0.2f, 1.0f };
-GLfloat point_green[] = { 0.2f, 1.0f, 0.2f, 1.0f };
-GLfloat point_blue[] = { 0.2f, 0.2f, 1.0f, 1.0f };
-GLfloat point_yellow[] = { 1.0f, 1.0f, 0.2f, 1.0f };
-// Initiate the color of point light
-GLfloat* point_color = point_white;			// Pointer for current color of point light
+	// Initiate cut off degree for spot light
+	GLfloat spot_cutoff = 8.6f;					// Cut off degree of spot light
+
+												// Define colors for lights
+	GLfloat point_white[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat point_red[4] = { 1.0f, 0.2f, 0.2f, 1.0f };
+	GLfloat point_green[4] = { 0.2f, 1.0f, 0.2f, 1.0f };
+	GLfloat point_blue[4] = { 0.2f, 0.2f, 1.0f, 1.0f };
+	GLfloat point_yellow[4] = { 1.0f, 1.0f, 0.2f, 1.0f };
+
+	// Initiate the color of point light
+	GLfloat* point_color = point_white;
+}Myscene;
+
+extern Myscene scene;
 
 // Define enum for menu items
 enum {
@@ -86,3 +95,32 @@ enum {
 	DISABLE,
 	EXIT
 };
+
+/*Draw.cpp*/
+void drawLeg();
+void drawScene();
+GLint genTableList();
+void drawLight(GLfloat* center, GLfloat radius);
+
+/*Light.cpp*/
+void initLight();
+void initPoint();
+void updatePoint();
+void initSpot();
+void updateSpot();
+
+/*Util.cpp*/
+void callList();
+void updateList();
+void updateCamera();
+void showSysStatus();
+
+/*System.cpp*/
+void idle();
+void initMenu(void);
+void processMenu(int value);
+void redraw();
+void reshape(int width, int height);
+void updateView(int width, int height);
+void processNormalKey(unsigned char k, int x, int y);
+void processSpecialKey(int k, int x, int y);
